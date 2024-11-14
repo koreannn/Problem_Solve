8115a8e4af9a9b15798e5a2bef63a965ada38cb9
koreannn <ghdtjdwo5@gamil.com>
2024-11-14 23:26:20 +0900

    다른풀이(num_dict)

--- programmers/python/숫자문자열과영단어/숫자문자열과영단어.py
+++ programmers/python/숫자문자열과영단어/숫자문자열과영단어.py
@@ -1,42 +1,19 @@
 def solution(s):
+    num_dict = {
+        "zero": "0", "one": "1", "two": "2", "three": "3", "four": "4",
+        "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"
+    }
     answer = []
     i = 0
     while i < len(s):
         if s[i].isdigit():
             answer.append(s[i])
             i += 1
-        elif s[i:i+4] == 'zero':
-            answer.append('0')
-            i += 4
-        elif s[i:i+3] == 'one':
-            answer.append('1')
-            i += 3
-        elif s[i:i+3] == 'two':
-            answer.append('2')
-            i += 3
-        elif s[i:i+5] == 'three':
-            answer.append('3')
-            i += 5
-        elif s[i:i+4] == 'four':
-            answer.append('4')
-            i += 4
-        elif s[i:i+4] == 'five':
-            answer.append('5')
-            i += 4
-        elif s[i:i+3] == 'six':
-            answer.append('6')
-            i += 3
-        elif s[i:i+5] == 'seven':
-            answer.append('7')
-            i += 5
-        elif s[i:i+5] == 'eight':
-            answer.append('8')
-            i += 5
-        elif s[i:i+4] == 'nine':
-            answer.append('9')
-            i += 4
         else:
-            i += 1
+            for word in num_dict:
+                if s[i:i+len(word)] == word:
+                    answer.append(num_dict[word])
+                    i += len(word)
     return int(''.join(answer))
 
 # test
