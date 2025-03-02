fce51822a0dad20eca8b6d23009039c27057e4d8
koreannn <ghdtjdwo5@gamil.com>
2025-03-02 15:19:26 +0900

    250302

--- boj/python/S3_피보나치함수/1003.py
+++ boj/python/S3_피보나치함수/1003.py
@@ -5,7 +5,6 @@ answer = []
 dp = [(1, 0), (0, 1)]  # 초기값(fibonacci(0), fibonacci(1)에서의 0, 1개수)
 
 for i in range(2, 41):
-    # i번째 피보나치 수의 0,1 호출 횟수는 i-1번째와 i-2번째 호출 횟수의 합
     zero = dp[i-1][0] + dp[i-2][0]
     one = dp[i-1][1] + dp[i-2][1]
     dp.append((zero, one))
