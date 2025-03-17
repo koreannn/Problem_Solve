699e6e0c84123e5f7886005bbf216792540a1bc2
koreannn <ghdtjdwo5@gamil.com>
2025-03-17 19:02:53 +0900

    250317

--- boj/python/B1_고양이는많을수록좋다/27961.py
+++ boj/python/B1_고양이는많을수록좋다/27961.py
@@ -1,29 +1,23 @@
-curr = 0 # 현재 가지고있는 고양이 마리수
-answer = 0 # 행동 횟수
+"""
+마법1. 고양이 1마리 생성
+마법2. 고양이 일부 또는 전부를 복제 (e.g. 현재 k마리가 존재 -> 0부터 k까지의 마리를 복제할 수 있음)
+똑같음
+"""
 
-N = int(input()) # 목표 고양이 마리수 (갱신되는값으로 사용)
+N = int(input())
+action_count = 1
+curr = 1 # 일단 생성 마법 한번은 쓰고 시작해야함
 
-while(N != 0): # N값이 0이될때까지 반복
-    if N == 0:
-        answer += 1
+while True:
+    if N == 1:
         break
-    # elif N == 1: 
-    #     answer += 1
-    #     break
-    else: # N >= 2일 경우
-        if curr == 0:
-            curr+=1
-            answer+=1
-            N = N-1
-            continue
-            
-        min_val = float('inf')
-        for i in range(0, curr+1): # 0이상 curr이하
-            if abs(N-i) < min_val:
-                min_val = abs(N-i) # 최적의 값 찾기 위한 값
-                add_val = i # 실제로 더해지는 고양이의 마리수
-        N = N-add_val
-        curr += add_val
-        answer+=1
-
-print(answer) # 최소한의 행동 횟수
+    
+    if N-curr > curr:
+        N -= curr
+        curr *= 2
+        action_count += 1
+    else:
+        action_count += 1
+        break
+        
+print(action_count)
--- boj/python/B1_부녀회장이될테야/2775.py
+++ boj/python/B1_부녀회장이될테야/2775.py
@@ -1,9 +1,24 @@
-test_case = int(input())
+"""
+점화식: floor[0][1]: 0층 2호에 사는 사람의 수
+k=1, n=3
+"""
 
-for _ in range(test_case):
-    k = int(input()) # 층
-    n = int(input()) # 호
+T = int(input())
+answer = []
+
+for _ in range(T):
+    k = int(input()) # 층수
+    n = int(input()) # 호수
+    people = []
+    people.append([i for i in range(1, n+1)]) # 0층 채우기 (k=0)
+
+    for i in range(1, k+1): # 1층부터 채우기 (k >= 1)
+        floor = []
+        for j in range(n): # 호수
+            floor.append(sum(people[-1][:j+1])) # 바로 아랫집(아래층 같은호수)까지
+        people.append(floor)
     
-    apt = [[0]*(n+1) for _ in range(k+1)] # 몇 명 사는지 초기화를 위한 초기 배열
-    
-    
+    answer.append(people[k][n-1])
+
+for ans in answer:
+    print(ans)
