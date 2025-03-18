33f384e6462e4b2963f7a78a8b5e0079657c222e
koreannn <ghdtjdwo5@gamil.com>
2025-03-19 01:18:49 +0900

    250319

--- boj/python/S5_집합/11723.py
+++ boj/python/S5_집합/11723.py
@@ -1,31 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input())
 S = set()
 
-M = int(input()) # 수행해야하는 연산의 수
-answer = []
-
 for _ in range(M):
-    command = input().stript().split()
-    if command[0] == 'add':
-        S.add(command[1])
-        
-    elif command[0] == 'remove':
-        S.discard(command[1]) # remove 말고 discard() 사용 -> KeyError 방지
+    command = input().strip().split()
     
-    elif command[0] == 'check':
-        answer.append(1 if command[1] in S else 0)
-    
-    elif command[0] == 'toggle':
-        if  command[1] in S:
-            S.remove(command[1])
+    if command[0] == "add":
+        S.add(int(command[1]))
+    elif command[0] == "remove":
+        S.discard(int(command[1]))
+    elif command[0] == "check":
+        if int(command[1]) in S:
+            print(1)
         else:
-            S.add(command[1])
-    
-    elif command[0] == 'all':
-        # S = set(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
-        S = set(range(1,21))
-    
-    elif command[0] == 'empty':
+            print(0)
+    elif command[0] == "toggle":
+        if int(command[1]) in S:
+            S.discard(int(command[1]))
+        else:
+            S.add(int(command[1]))
+    elif command[0] == "all":
+        S = set(range(1, 21))
+    elif command[0] == "empty":
         S.clear()
-
-for ans in answer:
-    print(ans)
--- boj/python/S5_팩토리얼0의개수/1676.py
+++ boj/python/S5_팩토리얼0의개수/1676.py
@@ -1,23 +1,12 @@
-# def fac(num):
-#     if num==1:
-#         return 1
-#     return num*fac(num-1)
+n = int(input())
 
-N = int(input())
 count = 0
+factorial = 1
 
-# str_factorial = str(fac(N))
+for num in range(n, 0, -1):
+    factorial = factorial*num
+    if (factorial%10 == 0):
+        factorial = factorial//10
+        count += 1
 
-# for i in range(len(str(str_factorial))-1, -1, -1):
-#     if str(str_factorial)[i] != '0':
-#         break
-#     count+=1
-for i in range(1, N+1):
-    if i%5 == 0:
-        count+=1
-    if i%25 == 0: # elif로 하면 안됨~
-        count+=1
-    if i%125 == 0:
-        count+=1
-    
 print(count)
