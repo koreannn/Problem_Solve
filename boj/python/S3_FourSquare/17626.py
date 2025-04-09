2e315cd54fec15316a38dfcae9928d6bcf964f49
koreannn <ghdtjdwo5@gamil.com>
2025-04-09 12:44:38 +0900

    250409

---
+++ boj/python/S3_FourSquare/17626.py
@@ -0,0 +1,25 @@
+"""
+4개 이하의 자연수 제곱합으로 만들 수 있음
+1<= n <= 50,000
+
+1 = 1^2
+2 = 1^2 + 1^2
+3 = 1^2 + 1^2 + 1^2
+4 = 2^2
+5 = 2^2 + 1^2
+6 = 2^2 + 1^2 + 1^2
+7 = 2^2 + 1^2 + 1^2 + 1^2
+8 = 2^2 + 2^2
+"""
+n = int(input())
+answer_list = [float('inf')]*(n+1)
+answer_list[0] = 0
+
+for i in range(1, n+1):
+    j = 1
+    while j**2 <= i:
+        answer_list[i] = min(answer_list[i], answer_list[i-j**2]+1)
+        j += 1
+    print(j**2)
+
+print(answer_list[n])
