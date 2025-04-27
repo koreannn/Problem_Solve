5d52734ee8cd265f573e7fba81ddd7f85185663a
koreannn <ghdtjdwo5@gamil.com>
2025-04-27 23:05:56 +0900

    250427

--- boj/python/S3_소수구하기/1929.py
+++ boj/python/S3_소수구하기/1929.py
@@ -1,20 +1,36 @@
 m, n = map(int, input().split()) # 1<= n, m <= 1,000,000
-answer = []
+# answer = []
 
-def is_prime(num: int) -> bool:
-    answer = False
-    for i in range(2, num):
-        if num%i == 0:
-            return answer
-    answer = True
-    return answer
+# def is_prime(num: int) -> bool:
+#     prime_TF = False
+#     for i in range(2, num):
+#         if num%i == 0:
+#             return prime_TF
+#     prime_TF = True
+#     return prime_TF
 
-for num in range(n, m+1):
-    if num == 1:
-        answer.append(str(num))
-        continue
-    if is_prime(num):
-        answer.append(str(num))
+# for num in range(m, n+1):
+#     if num == 1:
+#         continue
+#     if is_prime(num):
+#         answer.append(str(num))
 
-print(answer)
-print("\n".join(answer))
+def lin_sieve(n):
+    is_prime = [True] * (n+1)
+    primes = []
+    for num in range(2, n+1):
+        if is_prime[num]:
+            primes.append(num)
+        for p in primes:
+            if num*p > n:
+                break
+            is_prime[num*p] = False
+            if num%p == 0:
+                break
+    return primes
+
+primes = lin_sieve(n)
+
+for p in primes:
+    if p >= m:
+        print(p)
