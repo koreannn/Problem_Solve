42b5f1738e62650162c1dae7fe43a016ed7ae822
koreannn <ghdtjdwo5@gamil.com>
2025-05-27 08:10:35 +0900

    250526

--- boj/python/S3_문제/1072.py
+++ boj/python/S3_문제/1072.py
@@ -1,6 +1,9 @@
 x, y = map(int, input().split()) # 게임 수, 이긴 게임 수 
 z = y*100 // x # 승률 (e.g. 게임 판수: 53 / 이긴 게임 수: 47 ==> 47/53 약 88(소숫점 버림))
 
+def get_new_rate(x, y, mid):
+    return (y+mid)*100 // (x+mid)
+
 if z >= 99:
     print(-1)
 else:
@@ -10,7 +13,7 @@ else:
     
     while left <= right:
         mid = (left + right) // 2
-        new_z = (y+mid)*100 // (x+mid)
+        new_z = get_new_rate(x, y, mid)
         
         if new_z > z: # 더 작은 값이 있는지 탐색
             answer = mid
