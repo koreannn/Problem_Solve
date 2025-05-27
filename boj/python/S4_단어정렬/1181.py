7a014ca285802092136801bbc17397214a10193d
koreannn <ghdtjdwo5@gamil.com>
2025-05-28 07:51:30 +0900

    풀이 정제화

--- boj/python/S4_단어정렬/1181.py
+++ boj/python/S4_단어정렬/1181.py
@@ -1,16 +1,6 @@
-N = int(input())
-word_list = []
-tmp = ''
+n = int(input()) # 단어의 개수
+words = set(input() for _ in range(n))
+sorted_words = sorted(words, key=lambda x: (len(x), x))
 
-for _ in range(N):
-    word = input()
-    word_list.append(word)
-
-unique_list = set(word_list)
-sorted_word_list = sorted(unique_list, key=lambda x: (len(x), x))
-
-for word in sorted_word_list:
+for word in sorted_words:
     print(word)
-
-
-
