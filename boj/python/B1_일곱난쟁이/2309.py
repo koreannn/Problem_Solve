a9e1a62fd4853a11c98a50ca5ee9dcdd71ad134b
koreannn <ghdtjdwo5@gamil.com>
2025-06-07 14:34:45 +0900

    다른풀이(combination을 이용한 풀이)

--- boj/python/B1_일곱난쟁이/2309.py
+++ boj/python/B1_일곱난쟁이/2309.py
@@ -1,25 +1,13 @@
-heights = [int(input()) for _ in range(9)]
-sorted_heights = sorted(heights)
-check = False
-height1 = 0
-height2 = 0
+from itertools import combinations
 
-for i in range(len(sorted_heights)-1, -1, -1):
-    heights_sum = sum(sorted_heights)
-    height1 = sorted_heights[i]
-    heights_sum = heights_sum - height1
-    for j in range(i, -1, -1):
-        height2 = sorted_heights[j]
-        if (heights_sum - height2) < 100:
-            continue
-        if (heights_sum - height2) == 100:
-            check = True
-            break
-        
-    if check:
+heights = [int(input()) for _ in range(9)]
+total = sum(heights)
+
+for a, b in combinations(heights, 2): # 9명 중 2명을 뽑아서 제거해봄
+    if total - a - b == 100:
+        heights.remove(a)
+        heights.remove(b)
         break
 
-for h in sorted_heights:
-    if (h == height1) or (h == height2):
-        continue
+for h in sorted(heights):
     print(h)
