81a1bc1c9b386e5d3811e70dffc5722e59fc4bb3
koreannn <ghdtjdwo5@gamil.com>
2025-06-16 21:27:41 +0900

    250616

--- boj/python/S4_체스판다시칠하기/1018.py
+++ boj/python/S4_체스판다시칠하기/1018.py
@@ -1,23 +1,28 @@
-'''
-(0,0) ~ (N-8, M-8)까지 8*8짜리 체스판을 탐색
-'''
-N, M = map(int, input().split())  # NxM의 판때기 (N이 행의 개수)
-board = []
+n, m = map(int, input().split()) # 8이상 50이하
+board = [input() for _ in range(n)] # 한 행씩 칠하기
 
-for _ in range(N):
-    board.append(input())
+min_repaint = float('inf')
 
-min_val = 32
-
-for x in range(N-7):
-    for y in range(M-7):
-        # 8x8 영역의 각 행을 슬라이싱
-        test_board = [row[y:y+8] for row in board[x:x+8]]
-        white = sum(row.count('W') for row in test_board)
+for i in range(n-7): # 세로 시작 지점 (몇 번째 열?)
+    for j in range(m-7): # 가로 시작 지점 (몇 번째 행?)
+        count_b = 0 
+        count_w = 0 # 흰색을 검은색으로 바꾸는 경우, 검은색을 흰색으로 바꾸는 경우 둘 다 센 다음, 더 작은 값을 갖는 경우로 선택
         
-        if 32-white < min_val:
-            min_val = 32-white
+        for x in range(8):
+            for y in range(8):
+                curr = board[i+x][j+y] # 색칠을 할 지점
+                
+                if (x+y)%2 == 0: # 시작점을 기준으로 0, 2, 4, 6 열 또는 행
+                    if curr!='B': 
+                        count_b += 1
+                    if curr!='W':
+                        count_w += 1
+                else: # 시작점을 기준으로 1, 3, 5, 7 열 또는 행
+                    if curr!='W':
+                        count_b += 1
+                    if curr!='B':
+                        count_w += 1
         
-print(min_val)
+        min_repaint = min(min_repaint, count_b, count_w)
 
-
+print(min_repaint)
