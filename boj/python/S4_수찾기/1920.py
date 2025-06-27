2500b8cb135c733bf67b2fd56613a4f532f6f55c
koreannn <ghdtjdwo5@gamil.com>
2025-06-27 15:00:12 +0900

    이진탐색을 통한 풀이(다른 풀이)

--- boj/python/S4_수찾기/1920.py
+++ boj/python/S4_수찾기/1920.py
@@ -1,11 +1,27 @@
-N = int(input())
-N_list = list(map(int, input().split()))
+n = int(input())
+ground_truth = sorted(map(int, input().split()))
+m = int(input())
+query = map(int, input().split())
+answer = []
 
-M = int(input())
-M_list = list(map(int, input().split()))
-
-for num in M_list:
-    if num in N_list:
-        print("1")
+for num in query:
+    left, right = 0, n-1
+    finded = False
+    
+    while left<=right:
+        mid = (left+right)//2
+        
+        if num == ground_truth[mid]:
+            finded = True
+            break
+        elif num > ground_truth[mid]:
+            left = mid+1
+        else:
+            right = mid-1
+    
+    if finded:
+        answer.append("1")
     else:
-        print("0")
+        answer.append("0")
+    
+print('\n'.join(answer))
