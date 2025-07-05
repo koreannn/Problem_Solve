89d181db2e5319fc8de0167de59c845f2443f646
koreannn <ghdtjdwo5@gamil.com>
2025-07-05 15:56:13 +0900

    250705

--- boj/python/S3_이친수/2193.py
+++ boj/python/S3_이친수/2193.py
@@ -1,39 +1,12 @@
-def binary_translate(num: int, length: int) -> str:
-    binary = []
-    
-    curr = length
-    while num > 0:
-        div=2**(curr-1)
-        binary.append(str(num//div))
-        num = num%div
-        curr -= 1
-        
-    if len(binary) < length:
-        for _ in range(length - len(binary)):
-            binary.append('0')
-            
-    return ''.join(binary)
-
-
-def check_continuous(num_string: str) -> bool:
-    check = False
-    curr = num_string[0]
-    for i in range(1, len(num_string)):
-        if num_string[i] == curr and num_string[i] == '1':
-            check = True
-            return check
-        curr = num_string[i]
-    
-    return check
-
-
 n = int(input())
 answer = 0
-
-for num in range(1, 2**n): # e.g. n=3 -> 1, 2, 3, 4, 5, 6, 7
-    curr = binary_translate(num, n)
-    if (curr[0] == '0') or curr == check_continuous(curr):
-        continue
-    answer += 1
-        
-print(answer)
+if n <= 2:
+    answer = 1
+else:
+    dp = [0]*(n+1)
+    dp[1], dp[2] = 1, 1
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    answer = dp[n]
+    
+    print(answer)
---
+++ boj/python/S5_분수찾기/1193.py
@@ -0,0 +1,31 @@
+x = int(input())
+fractions = ['0']*x
+goal = 1
+curr_sum = 2
+start_check = False # True일 경우, 짝수번쨰 / False일 경우 홀수번째
+count = 0
+
+while count < x:
+    check = 1
+    
+    for j in range(count, goal):
+        if start_check: # 짝수번쨰일 경우
+            fractions[j] = f"{check}/{curr_sum-check}"
+        else: # 홀수번쨰일 경우
+            fractions[j] = f"{curr_sum-check}/{check}"
+        check+=1
+    
+    count += 1
+    curr_sum += 1
+    goal += 1
+    start_check *= -1
+    
+print(fractions[x-1])
+
+"""
+1/1
+1/2 2/1
+3/1 2/2 1/3
+1/4 2/3 3/2 4/1
+
+"""
