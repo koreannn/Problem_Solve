e25eff06b0ecf614736457c41633743d73fba0fc
koreannn <ghdtjdwo5@gamil.com>
2025-07-06 12:28:43 +0900

    두 원의 관계(참고)

--- boj/python/S3_터렛/1002.py
+++ boj/python/S3_터렛/1002.py
@@ -5,14 +5,15 @@ answer = []
 
 for _ in range(t):
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    dist = sqrt((x1-x2)**2 + (y1-y2)**2) # 두 점 사이의 거리
     
-    if (x1 == x2) and (y1 == y2) and (r1 == r2): # 두 원이 정확히 일치할 경우
+    if x1 == x2 and y1 == y2 and r1 == r2:
         answer.append("-1")
-    elif sqrt((x1-x2)**2 + (y1-y2)**2) == float(r1 + r2) or sqrt((x1-x2)**2 + (y1-y2)**2) == float(abs(r1 - r2)): # 접함(외접하거나, 내접하거나)
+    elif (abs(r1-r2) == dist) or (r1+r2 == dist):
         answer.append("1")
-    elif float(abs(r1-r2)) < sqrt((x1-x2)**2 + (y1-y2)**2) < float(r1 + r2): # 두 점에서 만날 경우 
-        answer.append("2")
+    elif abs(r1-r2) < dist and dist < r1 + r2:
+        answer.append("2")    
     else:
         answer.append("0")
-    
+
 print('\n'.join(answer))
