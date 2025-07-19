a5c0778a542a9c2a61a1fd4fc3953a7d52c3d159
koreannn <ghdtjdwo5@gamil.com>
2025-07-19 14:57:41 +0900

    250718

--- boj/python/S3_프린터큐/1966.py
+++ boj/python/S3_프린터큐/1966.py
@@ -1,10 +1,32 @@
 """
 중요도 숫자가 높을수록 중요도가 높음
+타겟 문서 뒤로, 해당 문서의 중요도보다 높은 종요도의 문서가 없으면 됨
+정렬 기준: 중요도가 높은 순서대로 & 중요도가 동일할 경우, 인덱스값이 작은 순서대로
 
 """
+import sys
+from collections import deque
+input = sys.stdin.readline
 
-test_case = int(input())
+t = int(input())
+answer = []
 
-for _ in range(test_case):
-    num_of_docs, query = map(int, input().split()) # 주어지는 문서의 개수, 물어볼 문서의 이름
-    score = int(input()) # 'query' 문서의 중요도 점수
+for _ in range(t):
+    n, m = map(int, input().split()) # n: 문서의 개수 / m: 찾고자 하는 문서가 몇번째에 놓여있는지를 나타내는 정수
+    sequences = list(map(int, input().split())) # 각 문서의 중요도 (중요도값이 클수록 더 먼저 출력됨)
+    queue = deque([(idx, priority) for idx, priority in enumerate(sequences)])
+    
+    count = 0
+    while queue:
+        curr = queue.popleft()
+        if any(curr[1] < q[1] for q in queue): # 맨 앞의 문서보다 중요도가 높은 문서가 있을 경우
+            queue.append(curr) # 뒤로 가기
+        else:
+            count += 1
+            if curr[0] == m: # 찾고자 하는 문서라면
+                break
+    
+    answer.append(str(count))
+
+
+print('\n'.join(answer))
