e818838f7acc1c2262ef6fa6fc3cd97c9ffc206a
koreannn <ghdtjdwo5@gamil.com>
2025-10-11 06:20:59 +0900

    251011

--- boj/python/S3_퇴사/14501.py
+++ boj/python/S3_퇴사/14501.py
@@ -1,17 +1,23 @@
-n = int(input())
-t_list = []
-p_list = []
-dp = [0]*(n+1) # dp[i]: i번째 날에 퇴사직전까지 가능한 최대 수익
+answer = 0
+N = int(input())
+period_list = [0]*(N+1)
+reward_list = [0]*(N+1)
+dp = [0]*(N+2)
 
-for _ in range(n):
-    t, p = map(int, input().split())
-    t_list.append(t)
-    p_list.append(p)
+for i in range(1, N+1):
+    period, reward = map(int, input().split())
+    period_list[i] = period
+    reward_list[i] = reward
+
+if period_list[N] + N == N+1: # 마지막날에 상담이 가능한 경우만
+    dp[N] = reward_list[N]
+
+for curr_day in range(N-1, 0, -1): # e.g. N = 7 -> 7, 6, 5, ... 1
+    consultation_end_day = curr_day + period_list[curr_day]
     
-for i in range(n-1, -1, -1): # n-1, n-2, ... 0
-    if (n-i) < t_list[i]: # 상담 기긴이 남은 퇴사일보다 큰 경우(상담 불가)
-        dp[i] = dp[i+1]
-    else: 
-        dp[i] = max(dp[i+1], p_list[i]+ dp[i + t_list[i]])
+    if consultation_end_day > N+1: # 상담 불가능한 경우
+        dp[curr_day] = dp[curr_day+1]
+    else: # N+1인 경우도 통과되기떄문에 인덱스 에러 발생
+        dp[curr_day] = max(dp[curr_day+1], reward_list[curr_day] + dp[consultation_end_day])
 
-print(dp[0])
+print(dp[1]) # from 첫번째날 to 마지막날 전체 최적 해
