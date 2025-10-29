e6da237b7bfee5111ea98000c78a1685470d0785
koreannn <ghdtjdwo5@gamil.com>
2025-10-29 17:23:52 +0900

    251029

--- boj/python/S2_유기농배추/1012.py
+++ boj/python/S2_유기농배추/1012.py
@@ -1,29 +1,33 @@
-from collections import deque
+"""
+지렁이가 있는 배추 -> 인접한 배추까지 안전 -> 인접한 배추의 인접한 배추까지 싹 다 안전
+
+구하는것: 필요한 최소 지렁이 수
+"""
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+def dfs(x, y, position):
+    for i in range(4):
+        next_x, next_y = x + dx[i], y + dy[i]
+        
+        if (next_x, next_y) in position:
+            position.remove((next_x, next_y))
+            dfs(next_x, next_y, position)
+    return
 
 T = int(input())
-answer_list = []
-
-dx = [0, 0, -1, 1] # 좌, 우 이동방향
-dy = [1, -1, 0, 0] # 상, 하 이동방향
-
 for _ in range(T):
-    M, N, K = map(int, input().split()) # 가로, 세로, 배추 수
-    field = [[0]*M for _ in range(N)] # 초기 배추밭
-    visited = [[False]*M for _ in range(N)] # 방문한곳 표기용
-
+    M, N, K = map(int, input().split())
+    position = set() # 배추가 위치한 좌표
+    
     for _ in range(K):
         x, y = map(int, input().split())
-        field[y][x] = 1 # 배추 위치
-        
-    def bfs(x, y):
-        queue = deque()
-        queue.append((x, y))
-        visited[y][x] = True
+        position.add((x, y))
+    
+    answer = 0
+    while position:
+        curr = next(iter(position))
+        position.remove(curr)
+        dfs(curr[0], curr[1], position)
+        answer += 1
 
-        while queue:
-            cx, cy = queue.popleft()
-            for i in range(4):
-                nx = cx + dx[i]
-                ny = cy + dy[i]
-                if (0 <= nx < M) and (0<= ny < N):
-                    
+    print(answer)
