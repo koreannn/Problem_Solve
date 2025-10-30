976aa6d660fc16350fec8ffd67066b0018e2d489
koreannn <ghdtjdwo5@gamil.com>
2025-10-30 11:49:34 +0900

    251029

--- boj/python/S4_수들의합2/2003.py
+++ boj/python/S4_수들의합2/2003.py
@@ -1,54 +1,31 @@
-N, M = map(int, input().split()) # 1<= <= 10,000 / 1<= <= 300,000,000
-N_list = list(map(int, input().split()))
-answer = 0
-
-# 올바른 풀이
+N, M = map(int, input().split())
 left, right = 0, 0
-curr = 0
+answer = 0
+curr_sum = 0
 
-while True:
-    if curr >= M:
-        if curr == M:
-            answer += 1
-        curr -= N_list[left]
-        left += 1
+nums = list(map(int, input().split()))
+
+for right in range(N):
+    curr_sum += nums[right]
     
-    else:
-        if right == N:
-            break
-        curr += N_list[right]
-        right += 1
+    while curr_sum >= M:
+        if curr_sum == M:
+            answer += 1
+        curr_sum -= nums[left]
+        left += 1
 
 print(answer)
 
-# dfs를 이용한 풀이 (시간복잡도떄문에 안됨)
-# def dfs(curr_idx, curr_sum):
+
+# while right <= N:
+#     curr_sum = sum(nums[left : right]) # 슬라이싱 및 sum연산 -> 비효율적
+    
 #     if curr_sum == M:
-#         global answer
-#         answer += 1 
-#         return
-#     if curr_idx == N or curr_sum > M:
-#         return
-    
-#     dfs(curr_idx + 1, curr_sum + N_list[curr_idx])
-    
-# for start in range(N):
-#     dfs(start, 0)
-
+#         answer += 1
+#         right += 1
+#     elif curr_sum < M:
+#         right += 1
+#     else: # curr_sum > M
+#         left += 1
+        
 # print(answer)
-
-# 위 dfs가 단순 반복문이랑 뭐가 다름?
-# for start in range(N):
-#     curr_sum = N_list[start]
-#     for j in range(start + 1, N):
-#         if curr_sum == M:
-#             answer += 1
-#             break
-#         if curr_sum > M:
-#             continue
-#         curr_sum += N_list[j]
-
-# print(answer)
-
-
-
