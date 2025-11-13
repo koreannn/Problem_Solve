eea896e99a487cf293a8b3dd2016205a2c6f06f9
koreannn <ghdtjdwo5@gamil.com>
2025-11-13 17:19:21 +0900

    251113

--- boj/python/S3_숫자정사각형/1051.py
+++ boj/python/S3_숫자정사각형/1051.py
@@ -1,16 +1,17 @@
-N, M = map(int, input().split()) # 가로 길이, 세로 길이
-nums = []
+N, M = map(int, input().split()) # 세로 길이, 가로 길이
+nums = [input().strip() for _ in range(N)]
 start_length = min(N, M) # 가장 큰 정사각형의 길이
 answer = 0
 
-for _ in range(M):
-    curr_nums = input()
-    nums.append()
-
 for curr_length in range(start_length, 0, -1): # e.g. 3, 2, 1
-    for row_start in range(M - start_length + 1): # 행 이동(위아래 이동)
-        for col_start in range(N - start_length + 1): # 열 이동(좌우 이동)
-            if (nums[row_start][col_start] == nums[row_start][col_start + start_length] == nums[row_start + start_length][col_start] == nums[row_start + start_length][col_start + start_length]) and curr_length > answer:
-                answer = curr_length
-    
-print(answer)
+    for col_start in range(M - curr_length + 1): # 열 이동(좌우 이동)
+        for row_start in range(N - curr_length + 1): # 행 이동(위아래 이동)
+            # 정사각형의 네 꼭지점
+            lt = nums[row_start][col_start] 
+            lb = nums[row_start + curr_length - 1][col_start]
+            rt = nums[row_start][col_start + curr_length - 1]
+            rb = nums[row_start + curr_length - 1][col_start + curr_length - 1]
+            
+            if lt == lb == rt == rb:
+                print(curr_length ** 2)
+                exit(0)
