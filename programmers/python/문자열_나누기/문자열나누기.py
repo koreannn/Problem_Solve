be8a24e84e40dfb663c60a3bc87c06ef26b28103
koreannn <ghdtjdwo5@gamil.com>
2026-06-06 17:01:34 +0900

    260606

--- programmers/python/문자열_나누기/문자열나누기.py
+++ programmers/python/문자열_나누기/문자열나누기.py
@@ -1,14 +1,28 @@
 def solution(s):
     answer = 0
-    while True:
-        first_char = s[0]
-        first_char_cnt = 0
-        else_char_cnt = 0
-            for i in range(len(s)):
-                if first_char_cnt == else_char_cnt:
-                    answer += 1
-                    continue
-                
+    curr_first = None
+    first_cnt = 0
+    else_cnt = 0
     
+    for char in s:
+        # 새로운 부분이 시작되는 경우 첫 글자 지정
+        if curr_first is None:
+            curr_first = char
+            first_cnt = 1
+            continue
+        
+        if char == curr_first:
+            first_cnt += 1
+        else:
+            else_cnt += 1
+        
+        if first_cnt == else_cnt:
+            answer += 1
+            curr_first = None
+            first_cnt = 0
+            else_cnt = 0
+    
+    if curr_first is not None:
+        answer += 1
     
     return answer
